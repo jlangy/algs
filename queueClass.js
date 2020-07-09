76fe0610e308664b2f45facc0b1e393d3448f8a2
jon <jonathan-langlois@live.ca>
2020-07-09 02:40:24 -0700

    queue class

---
+++ queueClass.js
@@ -0,0 +1,23 @@
+function Queue() {
+  var collection = [];
+  this.print = function() {
+    console.log(collection);
+  };
+  // Only change code below this line
+  this.enqueue = function(el){
+    collection.push(el);
+  }
+  this.dequeue = function(){
+    return collection.shift();
+  }
+  this.front = function(){
+    return collection[0]
+  }
+  this.size = function(){
+    return collection.length;
+  }
+  this.isEmpty = function(){
+    return collection.length == 0
+  }
+  // Only change code above this line
+}
