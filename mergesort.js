646d4c9618f6d6bbd949942ba5ff2d8e7c52913d
Jon Langlois <jonathan-langlois@live.ca>
2020-07-15 08:46:42 -0700

    merge

---
+++ mergesort.js
@@ -0,0 +1,13 @@
+function mergeSort(left, right){
+  if(left.length == 1){
+    if(right.length == 1){
+      return left[0] < right[0] ? [left[0], right[0]] : [right[0], left[0]]
+    }
+    else {
+      
+    }
+  } else {
+    half = Math.round(arr.length / 2)
+    mergeSort()
+  }
+}
